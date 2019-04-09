d993e9aff40ff6ca9436989018fd5c8ee334de3c
loremlipsum <lipsum.webdevelop@gmail.com>
2019-04-09 13:05:51 +0300

    Бугагашенька

---
+++ source/scripts/tabs.js
@@ -0,0 +1,27 @@
+'use strict';
+
+(function() {
+
+  var buttonsTabs = document.querySelectorAll('.js-button-tabs');
+  var contentsTabs = document.querySelectorAll('.js-content-tabs');
+
+  for (var i = 0; i < buttonsTabs.length; i++) {
+    buttonsTabs[i].addEventListener('click', function (e) {
+      e.preventDefault();
+      var tab = e.currentTarget;
+      var atr = tab.getAttribute('data-tab');
+
+      for (var j = 0; j < contentsTabs.length; j++) {
+        contentsTabs[j].classList.remove('active');
+      };
+
+      for (var j = 0; j < buttonsTabs.length; j++) {
+        buttonsTabs[j].classList.remove('active');
+      };
+
+      tab.classList.add('active');
+      document.querySelector("." + atr).classList.add('active');
+    });
+  }
+
+})();
