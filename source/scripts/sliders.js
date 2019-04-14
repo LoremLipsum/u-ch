ab63f9b59cc1cd197041c92d367fa6d626542a6b
loremlipsum <lipsum.webdevelop@gmail.com>
2019-04-14 18:23:22 +0300

    add

--- source/scripts/sliders.js
+++ source/scripts/sliders.js
@@ -2,64 +2,107 @@
 	'use strict';
 	$(function() {
 
-    $('.js-slider').slick({
-      arrows: true,
-      dots: true,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      swipeToSlide: true,
-      autoplay: true,
-			autoplaySpeed: 5000,
-      infinite: true,
-      dotsClass: 'slick-dots',
-      prevArrow: '<button class="slider-button slider-button--prev" type="button" tabindex="0" aria-lable="Назад"></button>',
-      nextArrow: '<button class="slider-button slider-button--next" type="button" tabindex="0" aria-lable="Вперёд"></button>',
-    });
+    var sliderMain = $('.js-main-slider');
+    var sliderBrends = $('.js-brends-slider');
+    var sliderProductLine = $('.js-product-line-slider');
 
-    $('.js-brends').slick({
-      arrows: true,
-      dots: false,
-      slidesToShow: 5,
-      slidesToScroll: 1,
-      swipeToSlide: true,
-      autoplay: true,
-      autoplaySpeed: 5000,
-      infinite: true,
-      prevArrow: '<button class="brends__button brends__button--prev" type="button" tabindex="0" aria-lable="Назад"></button>',
-      nextArrow: '<button class="brends__button brends__button--next" type="button" tabindex="0" aria-lable="Вперёд"></button>',
-      responsive: [
-        {
-          breakpoint: 1020,
-          settings: {
-            slidesToShow: 4,
-          }
-        },
-        {
-          breakpoint: 890,
-          settings: {
-            slidesToShow: 3,
-          }
-        },
-        {
-          breakpoint: 768,
-          settings: {
-            slidesToShow: 4,
-          }
-        },
-        {
-          breakpoint: 620,
-          settings: {
-            slidesToShow: 3,
-          }
-        },
-        {
-          breakpoint: 520,
-          settings: {
-            slidesToShow: 1,
-          }
-        },
-      ]
-    });
+    if(sliderMain) {
+      sliderMain.slick({
+        arrows: true,
+        dots: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        swipeToSlide: true,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        infinite: true,
+        dotsClass: 'slick-dots',
+        prevArrow: '<button class="slider-button slider-button--prev" type="button" tabindex="0" aria-lable="Назад"></button>',
+        nextArrow: '<button class="slider-button slider-button--next" type="button" tabindex="0" aria-lable="Вперёд"></button>',
+      });
+    }
+
+    if(sliderBrends) {
+      sliderBrends.slick({
+        arrows: true,
+        dots: false,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        swipeToSlide: true,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        infinite: true,
+        prevArrow: '<button class="brends__button brends__button--prev" type="button" tabindex="0" aria-lable="Назад"></button>',
+        nextArrow: '<button class="brends__button brends__button--next" type="button" tabindex="0" aria-lable="Вперёд"></button>',
+        responsive: [
+          {
+            breakpoint: 1020,
+            settings: {
+              slidesToShow: 4,
+            }
+          },
+          {
+            breakpoint: 890,
+            settings: {
+              slidesToShow: 3,
+            }
+          },
+          {
+            breakpoint: 768,
+            settings: {
+              slidesToShow: 4,
+            }
+          },
+          {
+            breakpoint: 620,
+            settings: {
+              slidesToShow: 3,
+            }
+          },
+          {
+            breakpoint: 520,
+            settings: {
+              slidesToShow: 1,
+            }
+          },
+        ]
+      });
+    }
+
+    if(sliderProductLine) {
+      sliderProductLine.slick({
+        arrows: true,
+        dots: false,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        swipeToSlide: true,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        infinite: true,
+        prevArrow: '<button class="button-arrow button-arrow--prev" type="button" tabindex="0" aria-lable="Назад"></button>',
+        nextArrow: '<button class="button-arrow button-arrow--next" type="button" tabindex="0" aria-lable="Вперёд"></button>',
+        responsive: [
+          {
+            breakpoint: 1020,
+            settings: {
+              slidesToShow: 3,
+            }
+          },
+          {
+            breakpoint: 890,
+            settings: {
+              slidesToShow: 2,
+            }
+          },
+          {
+            breakpoint: 620,
+            settings: {
+              slidesToShow: 1,
+            }
+          },
+        ]
+      });
+    }
 
 	});
 })(jQuery);
--- source/scripts/tabs.js
+++ source/scripts/tabs.js
@@ -2,26 +2,34 @@
 
 (function() {
 
-  var buttonsTabs = document.querySelectorAll('.js-button-tabs');
+  var btnsTabs = document.querySelectorAll('.js-button-tabs');
   var contentsTabs = document.querySelectorAll('.js-content-tabs');
 
-  for (var i = 0; i < buttonsTabs.length; i++) {
-    buttonsTabs[i].addEventListener('click', function (e) {
-      e.preventDefault();
-      var tab = e.currentTarget;
-      var atr = tab.getAttribute('data-tab');
+  var btnProductTabs = document.querySelectorAll('.js-product-tabs-button');
+  var contentsProductTabs = document.querySelectorAll('.js-product-tabs-content');
 
-      for (var j = 0; j < contentsTabs.length; j++) {
-        contentsTabs[j].classList.remove('active');
-      };
+  var switchTabs = function(btn, content) {
+    for (var i = 0; i < btn.length; i++) {
+      btn[i].addEventListener('click', function (e) {
+        e.preventDefault();
+        var tab = e.currentTarget;
+        var atr = tab.getAttribute('data-tab');
 
-      for (var j = 0; j < buttonsTabs.length; j++) {
-        buttonsTabs[j].classList.remove('active');
-      };
+        for (var j = 0; j < content.length; j++) {
+          content[j].classList.remove('active');
+        };
 
-      tab.classList.add('active');
-      document.querySelector("." + atr).classList.add('active');
-    });
-  }
+        for (var j = 0; j < btn.length; j++) {
+          btn[j].classList.remove('active');
+        };
+
+        tab.classList.add('active');
+        document.querySelector("." + atr).classList.add('active');
+      });
+    }
+  };
+
+  switchTabs(btnsTabs, contentsTabs);
+  switchTabs(btnProductTabs, contentsProductTabs);
 
 })();
